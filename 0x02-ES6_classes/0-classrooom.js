83b0f225ff5a78ff9950dda3cbc3304751f25d9f
lilprof01 <adewumime@gmail.com>
2025-04-17 20:08:14 +0000

    testing

--- 0x02-ES6_classes/0-classrooom.js
+++ 0x02-ES6_classes/0-classrooom.js
@@ -3,7 +3,7 @@
  */
 export default class ClassRoom {
   /**
-   * Creates a new class object.
+   * Creates a new class object{@link ClassRoom}.
    * @param {Number} maxStudentsSize - The maximum number of
    * students in the class.
    */
