fa28b8a0afd1b9270039a78fb5ba3b4a18a26b3f
lilprof01 <adewumime@gmail.com>
2025-04-18 01:28:37 +0000

    advanced task

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,42 @@
+/**
+ * Creates a new car class
+ *
+ * @params: brand - car brand
+ * @params: motor - motor type
+ * @params: color - car color
+ * */
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
