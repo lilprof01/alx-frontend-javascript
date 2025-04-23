f16538909bcf39c0d8c06cfbf1e7356a1bf82d14
lilprof01 <adewumime@gmail.com>
2025-04-23 20:27:29 +0000

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,8 +2,8 @@
  * Function retrieves a list of students.
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
-export default const getListStudents = () => {
-  [
+export default function getListStudents() {
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
